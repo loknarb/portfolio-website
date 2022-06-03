d62cd73867121f8f45338b48b497ea4bc2c56371
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-03 22:16:20 +0200

    created quick header display

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import MenuLogo from './UI/MenuLogo';
 const Header = () => {
-  return <nav className='flex flex-'>s-brum</nav>;
+  return (
+    <nav className='flex items-center justify-between flex-1 px-3 py-3'>
+      <span className='text-5xl font-semibold -tracking-wider font-display'>s-brum</span>
+      <MenuLogo />
+    </nav>
+  );
 };
 
 export default Header;
