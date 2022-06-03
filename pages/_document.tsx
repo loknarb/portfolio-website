b7ca4b3a3e0de9468cfe8ae57a62cc571d659fcd
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-03 22:17:47 +0200

    adjusted with prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -10,21 +10,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'true'} />
           <link
-            href="https://fonts.googleapis.com/css2?family=Exo+2:wght@700&family=Orbitron:wght@700&family=Signika:wght@700&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&display=swap'
+            rel='stylesheet'
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@600&display=swap'
+            rel='stylesheet'
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Dosis&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap'
+            rel='stylesheet'
           />
-          <link rel="icon" href="/favicon.png" />
+          <link rel='icon' href='/favicon.png' />
         </Head>
         <body>
           <Main />
