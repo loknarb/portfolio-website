0682ba029019de5933a1c3af8d2e321f14d17e08
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-04 00:05:17 +0200

    working on displaying our versions inside our codeblock

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from 'next';
+import latestVersion from 'latest-version';
+const process = require('process');
 type Data = {
-  name: string
-}
+  node: string;
+  yarn: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const yarn = await latestVersion('yarn');
+  await res.status(200).json({ node: process.version, yarn: yarn });
 }
