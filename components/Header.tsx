471dace8789327430b7281537d7d97d041a55a5a
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-04 22:54:49 +0200

    imported state, hiding button after clicked

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { SetBooleanValue, UseBooleanValue } from './types/types';
 import MenuLogo from './UI/MenuLogo';
-const Header = () => {
+import PrimaryButton from './UI/PrimaryButton';
+const Header: React.FC<{ sideBar: UseBooleanValue; setSidebar: SetBooleanValue }> = ({
+  sideBar,
+  setSidebar,
+}) => {
   return (
-    <nav className='flex items-center justify-between flex-1 px-3 py-3'>
+    <nav className='flex items-center justify-between flex-1 h-24 px-3 py-3'>
       <span className='text-5xl font-semibold -tracking-wider font-display'>s-brum</span>
-      <MenuLogo />
+      {sideBar && (
+        <PrimaryButton onClick={() => setSidebar(false)}>
+          <MenuLogo />
+        </PrimaryButton>
+      )}
     </nav>
   );
 };
