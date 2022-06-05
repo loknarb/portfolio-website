f8c37257872fec0aa2071c838c6f87ea65f6336b
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 17:12:57 +0200

    made my talking points grid styled

--- components/GridPoints.tsx
+++ components/GridPoints.tsx
@@ -8,25 +8,25 @@ import WebDevIcon from './UI/WebDevIcon';
 
 const GridPoints = () => {
   return (
-    <div className='grid tablet:grid-cols-3 p-8 tablet:gap-14 gap-14 max-w-7xl w-[90%] mx-auto tablet:bg-foregroundNoise rounded'>
+    <div className='grid tablet:grid-cols-3 p-8 tablet:gap-14 gap-14 max-w-7xl w-[90%] mx-auto tablet:bg-foregroundNoise rounded my-4'>
       <PrimaryCard>
-        <WebDevIcon />
-        <H2Simple className='my-2 lowercase'>Web Development</H2Simple>
+        <WebDevIcon color='fill-yellow-200' className='fill-yellow-200' />
+        <H2Simple className='my-2 font-semibold lowercase'>Web Development</H2Simple>
         <ParagraphSimple className='text-center'>
           I will develop an scalable and responsive website with tools such as React, Nextjs,
           Express, ...etc.
         </ParagraphSimple>
       </PrimaryCard>
       <PrimaryCard>
-        <PaperIcon />
-        <H2Simple className='my-2 lowercase'>Brand Strategy</H2Simple>
+        <PaperIcon color='bg-pink-300' className='fill-pink-300' />
+        <H2Simple className='my-2 font-semibold lowercase'>Brand Strategy</H2Simple>
         <ParagraphSimple className='text-center'>
           We will figure out how best to exemplify your brand and push its awareness
         </ParagraphSimple>
       </PrimaryCard>
       <PrimaryCard>
-        <DesignIcon />
-        <H2Simple className='my-2 lowercase'>Web Design</H2Simple>
+        <DesignIcon color='fill-green-300' className='fill-green-300' />
+        <H2Simple className='my-2 font-semibold lowercase'>Web Design</H2Simple>
         <ParagraphSimple className='text-center'>
           You and I will design an aesthetically pleasing website, capable of being scaled.
         </ParagraphSimple>
