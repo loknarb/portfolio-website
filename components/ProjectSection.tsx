58eff401b352f3d12a261bbcf9b31b60a9317171
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 17:13:54 +0200

    rainbow line added along with hover animations shadow

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -7,22 +7,26 @@ import PrimaryButton from './UI/PrimaryButton';
 const ProjectSection = () => {
   return (
     <>
-      <div className='items-center w-full my-20 tablet:flex-row-reverse tablet:flex tablet:justify-evenly desktop:px-44'>
-        <BlackDesktop className='py-5 mx-auto tablet:py-0 tablet:mx-0 tablet:flex-1 tablet:flex-shrink-0 tablet:h-[400px] tablet:w-[400px] w-64 h-64 tablet:ml-8' />
-        <div className='max-w-4xl p-4 border rounded border-zinc-600 border-opacity-20 tablet:mx-auto'>
+      <div className='items-center w-full my-20 tablet:flex-row-reverse tablet:flex tablet:justify-evenly desktop:px-44 '>
+        <BlackDesktop className=' mx-auto tablet:py-0 tablet:mx-0 tablet:flex-1 tablet:flex-shrink-0 tablet:h-[400px] tablet:w-[400px] w-64 h-64 tablet:ml-8 -mb-6' />
+        <div className='max-w-4xl p-4 border rounded shadow-md border-zinc-600 border-opacity-20 tablet:mx-auto shadow-zinc-800'>
           <H2Simple>projects</H2Simple>
-          <FullLine className='tablet:w-60 ' />
+          <div className='flex tablet:w-60'>
+            <FullLine className='w-20 border-yellow-200' />
+            <FullLine className='w-20 border-pink-300' />
+            <FullLine className='w-20 border-green-300' />
+          </div>
           <div className='grid grid-cols-2 gap-2 w-[85%] mx-auto my-4 tablet:grid-cols-3 desktop:gap-20'>
-            <PrimaryButton className='hover:border hover:border-zinc-600'>
-              <span className='text-lg font-display -tracking-wider'>news-gallery</span>
+            <PrimaryButton className='justify-around tablet:h-32 hover:border hover:border-zinc-600 hover:shadow-pink-300/10'>
+              <span className='text-lg leading-4 font-display -tracking-wider'>news-gallery</span>
               <span className='text-sm font-body'>react / typescript </span>
             </PrimaryButton>
-            <PrimaryButton className='border rounded hover:border-zinc-600'>
-              <span className='text-lg font-display -tracking-wider'>stopwatch</span>
+            <PrimaryButton className='justify-around border rounded tablet:h-32 hover:border-zinc-600 hover:shadow-green-300/10'>
+              <span className='text-lg leading-4 font-display -tracking-wider'>stopwatch</span>
               <span className='text-sm font-body'>python / tkinter </span>
             </PrimaryButton>
-            <PrimaryButton className='border rounded hover:border-zinc-600'>
-              <span className='text-lg font-display -tracking-wider'>state-comparer</span>
+            <PrimaryButton className='justify-around border rounded tablet:h-32 hover:border-zinc-600 hover:shadow-yellow-200/10'>
+              <span className='text-lg leading-4 font-display -tracking-wider'>state-comparer</span>
               <span className='text-sm font-body'>react / typescript </span>
             </PrimaryButton>
           </div>
