48132ad79b88c0801b327eb1bcac0d8e6435a74d
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 18:32:48 +0200

    text increased on bigger monitors

--- components/UI/H2Simple.tsx
+++ components/UI/H2Simple.tsx
@@ -7,8 +7,8 @@ const H2Simple: React.FC<
     <h2
       className={
         className
-          ? `text-2xl tablet:ml-2 ml-4 font-bold font-display -tracking-wider ${className}`
-          : 'text-2xl tablet:ml-2 ml-4 font-bold font-display -tracking-wider'
+          ? `text-2xl desktop:text-4xl tablet:ml-2 ml-4 font-bold font-display -tracking-wider ${className}`
+          : 'text-2xl desktop:text-4xl tablet:ml-2 ml-4 font-bold font-display -tracking-wider'
       }
       {...rest}>
       {children}
