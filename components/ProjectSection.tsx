35bd3ebc1c625b33cdc6a4049ad052ea1378d8e6
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 13:37:58 +0200

    project section responsive

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -7,21 +7,27 @@ import PrimaryButton from './UI/PrimaryButton';
 const ProjectSection = () => {
   return (
     <>
-      <BlackDesktop className='py-5 mx-auto' />
-      <H2Simple>projects</H2Simple>
-      <FullLine className='tablet:w-60' />
-      <PrimaryButton className='hover:border hover:border-zinc-600'>
-        <span className='text-lg font-display -tracking-wider'>news-gallery</span>
-        <span className='text-sm font-body'>react / typescript </span>
-      </PrimaryButton>
-      <PrimaryButton className='border rounded hover:border-zinc-600'>
-        <span className='text-lg font-display -tracking-wider'>stopwatch</span>
-        <span className='text-sm font-body'>python / tkinter </span>
-      </PrimaryButton>
-      <PrimaryButton className='border rounded hover:border-zinc-600'>
-        <span className='text-lg font-display -tracking-wider'>state-comparer</span>
-        <span className='text-sm font-body'>react / typescript </span>
-      </PrimaryButton>
+      <div className='items-center w-full my-20 tablet:flex-row-reverse tablet:flex tablet:justify-evenly desktop:px-44'>
+        <BlackDesktop className='py-5 mx-auto tablet:py-0 tablet:mx-0 tablet:flex-1 tablet:flex-shrink-0 tablet:h-[400px] tablet:w-[400px] w-64 h-64 tablet:ml-8' />
+        <div className='max-w-4xl p-4 border rounded border-zinc-600 border-opacity-20 tablet:mx-auto'>
+          <H2Simple>projects</H2Simple>
+          <FullLine className='tablet:w-60 ' />
+          <div className='grid grid-cols-2 gap-2 w-[85%] mx-auto my-4 tablet:grid-cols-3 desktop:gap-20'>
+            <PrimaryButton className='hover:border hover:border-zinc-600'>
+              <span className='text-lg font-display -tracking-wider'>news-gallery</span>
+              <span className='text-sm font-body'>react / typescript </span>
+            </PrimaryButton>
+            <PrimaryButton className='border rounded hover:border-zinc-600'>
+              <span className='text-lg font-display -tracking-wider'>stopwatch</span>
+              <span className='text-sm font-body'>python / tkinter </span>
+            </PrimaryButton>
+            <PrimaryButton className='border rounded hover:border-zinc-600'>
+              <span className='text-lg font-display -tracking-wider'>state-comparer</span>
+              <span className='text-sm font-body'>react / typescript </span>
+            </PrimaryButton>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
