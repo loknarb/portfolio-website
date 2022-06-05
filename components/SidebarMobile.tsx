b82948fe9b1648f1b07e934e13effb26c9ebb2c4
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 18:32:01 +0200

    sidebar adjusted to play with nav bar

--- components/SidebarMobile.tsx
+++ components/SidebarMobile.tsx
@@ -1,3 +1,4 @@
+import ResumeLink from './ResumeLink';
 import { SetBooleanValue, UseBooleanValue } from './types/types';
 import ArrowRight from './UI/ArrowRight';
 import PrimaryButton from './UI/PrimaryButton';
@@ -9,12 +10,12 @@ const SidebarMobile: React.FC<{ sideBar: UseBooleanValue; setSidebar: SetBoolean
   return (
     <>
       <div
-        className={`fixed w-full bg-zinc-900 bg-opacity-50 z-30 cursor-pointer inset-0 ${
-          sideBar ? 'opacity-0 w-0 h-0' : 'opacity-100 w-full '
+        className={`fixed w-full bg-gradient-to-br from-yellow-200 via-pink-300 to-green-300 z-30 cursor-pointer inset-0 ${
+          sideBar ? 'opacity-0 w-0 h-0' : 'opacity-[0.05] w-full '
         }`}
         onClick={() => setSidebar(true)}></div>
       <aside
-        className={`p-4 top-0 right-0 h-42 w-32 z-40 flex-col flex fixed bg-transparent  shadow-sm  transistion-transform ease-in-out -translate-x-64 w-55 duration-300 rounded-bl  ${
+        className={`py-4 px-10 top-0 right-0 h-42 w-48 z-40 flex-col flex fixed bg-transparent  shadow-sm  transistion-transform ease-in-out -translate-x-64 w-55 duration-300 rounded-bl  ${
           sideBar ? 'translate-x-64' : 'translate-x-0'
         }`}>
         <div className='self-end'>
@@ -22,7 +23,9 @@ const SidebarMobile: React.FC<{ sideBar: UseBooleanValue; setSidebar: SetBoolean
             <ArrowRight />
           </PrimaryButton>
         </div>
-        <div className='p-4 rounded bg-zinc-800 bg-opacity-80'>Resume Link Will Go Here</div>
+        <div className='p-4 bg-opacity-100 rounded bg-backgroundNoise'>
+          <ResumeLink />
+        </div>
       </aside>
     </>
   );
