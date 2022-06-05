ab3c4b4a655d9abec2b7ada890fc13735593586e
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-05 13:38:26 +0200

    removed comptatibility future issue

--- components/UI/PrimaryButton.tsx
+++ components/UI/PrimaryButton.tsx
@@ -5,7 +5,7 @@ const PrimaryButton: React.FC<
 > = ({ onClick, className, children, ...rest }) => {
   return (
     <button
-      className={` transition-all border border-zinc-600 border-opacity-20 duration-500 rounded my-3 px-2 py-2 flex hover:scale-105 flex-col bg-transparent hover:shadow-md shadow-zinc-200 ${
+      className={` transition-all border border-zinc-600 border-opacity-20 duration-500 rounded px-2 py-2 flex hover:scale-105 flex-col bg-transparent hover:shadow-md shadow-zinc-200 ${
         className ? className : ''
       }`}
       onClick={onClick ? onClick : () => {}}
